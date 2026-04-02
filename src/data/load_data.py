7c2703aa686e5cfafa579bde34ad5043d90d43c0
Samu Barboza <samu_junior95@hotmail.com>
2026-04-02 17:46:06 -0300

    base de mejora para cargar csvs correctos, mejora de churn con regla de negocio, rehacer feature_engineering.py para generar mejores features a partir de los datos

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,6 +1,9 @@
+import os
 import pandas as pd
 from src.config.settings import DATA_RAW
 
+
 def load_csv(filename):
-    path = f"{DATA_RAW}{filename}"
+    """Carga un archivo CSV desde la carpeta de datos raw."""
+    path = os.path.join(DATA_RAW, filename)
     return pd.read_csv(path)
